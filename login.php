7b0b10cba448d33504eb5356fdaefdb48fe79d3b
Gustavo Camargo <gustavoregis@gmail.com>
2026-01-05 19:15:54 -0300

    feat: basic structures

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body class="dark">
+
+<div class="login-container">
+    <h1>Entrar</h1>
+
+    <form class="login-form">
+        <div class="form-group">
+            <label for="email">E-mail</label>
+            <input type="email" id="email" placeholder="seu@email.com">
+        </div>
+
+        <div class="form-group">
+            <label for="password">Senha</label>
+            <input type="password" id="password" placeholder="********">
+        </div>
+
+        <button type="button" class="btn-login">Entrar</button>
+
+        <div class="form-footer">
+            <a href="#">Esqueci minha senha</a>
+        </div>
+    </form>
+</div>
+
+</body>
+</html>
